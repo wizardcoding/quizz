256ba972b8bcbb173ca46b57ce251518f452171d
xayuzaii <wizardcodingmachine@gmail.com>
2024-07-30 15:06:02 -0600

    created the main screen

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "----------------- Welco to quizz -----------------\n" << endl;
+    cout << "--------------Follow the intructions--------------\n" << endl;
+    cout << "Step 1: Quiz contains total 10 questions\n" << endl;
+    cout << "Step 2: You Will given 1 mark for each right answer\n" << endl;
+    cout << "Step 3: There will be no negative marking\n" << endl;
+    cout << "Step 4: Please press s to start the quizz\n" << endl;
+
+
+}
